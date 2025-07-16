4d6c141c266cd93eb3851adfd6c782f9bcb420f1
Chrystine Martins <chrystine.martins@outlook.com>
2025-07-15 21:22:23 -0300

    add and update prompt-main

--- src/prompts/prompt-main.js
+++ src/prompts/prompt-main.js
@@ -1,11 +1,11 @@
-import { message } from "prompt";
-
 const mainPrompt = [
     {
         name: "select",
-        description: "Escolha a ferramenta (1- QR Code ou 2- Password)",
+        description: "Escolha a ferramenta: 1- QR Code ou 2- Password",
         pattern: /^[1-2]+$/,
         message: "Escolha inválida. Digite 1 para QR Code ou 2 para Password.",
         required: true,
     }
 ]
+
+export default mainPrompt;
