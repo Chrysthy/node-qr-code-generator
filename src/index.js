67822ca581efb2f57a5ac1da356de440b05c07a0
Chrystine Martins <chrystine.martins@outlook.com>
2025-07-20 19:54:15 -0300

    Import createQRCode()

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import prompt from 'prompt'
 import mainPrompt from './prompts/prompt-main.js'
+import createQRCode from './services/qr-code/create.js';
+
 
 async function main() {
 
     prompt.get(mainPrompt, async (err, choose) => {
 
-        if (choose.select == 1) console.log("Você escolheu QR Code");
+        if (choose.select == 1) await createQRCode();
 
         if (choose.select == 2) console.log("Você escolheu Password");
 
