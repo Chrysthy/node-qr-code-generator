cfecfd5a6959fbfa544c2f3743bf190fa1ab9315
Chrystine Martins <chrystine.martins@outlook.com>
2025-07-20 21:45:19 -0300

    add prompt qr code

--- src/prompts/prompt-qr-code.js
+++ src/prompts/prompt-qr-code.js
@@ -1,5 +1,19 @@
 import chalk from "chalk";
 
-const promptQRCode = []
+const promptQRCode = [
+
+    {
+        name: 'link',
+        description: chalk.yellow("Digite o link para gerar o QR Code")
+    },
+
+    {
+        name: 'type',
+        description: chalk.yellow("Escolha o tipo de QR Code: 1- Imagem, 2- Terminal"),
+        pattern: /^[1-2]+$/,
+        message: chalk.red.italic("Escolha inválida. Digite 1 para Imagem ou 2 para Terminal."),
+        required: true
+    }
+]
 
 export default promptQRCode;
