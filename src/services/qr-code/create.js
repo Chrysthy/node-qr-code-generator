6f2619c9cadf299610cc67057f7c10d7b3f16bbf
Chrystine Martins <chrystine.martins@outlook.com>
2025-07-22 19:28:11 -0300

    updat files name

--- src/services/qr-code/create.js
+++ src/services/qr-code/create.js
@@ -1,5 +1,5 @@
 import prompt from 'prompt';
-import promptQRCode from '../../prompts/prompt-qr-code.js';
+import promptQRCode from '../../prompts-schema/prompt-schema-qr-code.js';
 import handle from './handle.js';
 
 async function createQRCode() {
