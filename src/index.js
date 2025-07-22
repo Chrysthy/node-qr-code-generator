9aebb6724031081e1a70864127789187257cbab4
Chrystine Martins <chrystine.martins@outlook.com>
2025-07-21 21:28:24 -0300

    import file create password

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import prompt from 'prompt'
 import mainPrompt from './prompts/prompt-main.js'
 import createQRCode from './services/qr-code/create.js';
+import createPassword from './services/password/create.js';
 
 
 async function main() {
@@ -9,7 +10,7 @@ async function main() {
 
         if (choose.select == 1) await createQRCode();
 
-        if (choose.select == 2) console.log("Você escolheu Password");
+        if (choose.select == 2) await createPassword();
 
     })
 
