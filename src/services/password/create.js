a71c7c2f203710ac07764b793564a3c14ad85a6e
Chrystine Martins <chrystine.martins@outlook.com>
2025-07-21 21:46:32 -0300

    import handle password

--- src/services/password/create.js
+++ src/services/password/create.js
@@ -1,7 +1,12 @@
+import handle from "./handle";
+
 const { default: chalk } = require("chalk");
 
 async function createPassword() {
     console.log(chalk.blue("Password"));
+
+    const password = await handle();
+    console.log(password);  
     
 }
 
